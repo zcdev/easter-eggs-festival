68ae155d4bafbed53e6ce40672ac028abd517617
zcdev <zcdev@hotmail.com>
2025-07-06 17:20:22 -0700

    feat: implement Egg and EggList components with accessible markup

--- src/components/Egg.jsx
+++ src/components/Egg.jsx
@@ -1,6 +1,9 @@
-export default function Egg() {
+export default function Egg({ egg }) {
     return (
-        <>
-        </>
+        <li>
+            <button className="egg" aria-label={egg.clicked ? `Egg ${egg.id}, value: ${egg.value}` : `Egg ${egg.id}`}>
+                <img src={egg.image} alt="" aria-hidden="true" />
+            </button>
+        </li>
     )
 }
--- src/components/EggList.jsx
+++ src/components/EggList.jsx
@@ -1,6 +1,9 @@
-export default function EggList() {
+export default function EggList({ children }) {
     return (
-        <>
-        </>
+        <section aria-label="List of hidden eggs">
+            <ul>
+                {children}
+            </ul>
+        </section>
     )
 }
