d24c220995eb391e7cc0fafd0464577a62eea089
zcdev <zcdev@hotmail.com>
2025-07-27 05:32:00 -0700

    style: refactor Egg component for images
    Integrate properly sized and optimized images with WebP format

--- src/components/Egg.jsx
+++ src/components/Egg.jsx
@@ -1,8 +1,20 @@
 export default function Egg({ egg, onClick }) {
     return (
         <li>
-            <button className="egg" onClick={() => onClick(egg.value)}  aria-label={egg.clicked ? `Egg ${egg.id}, value: ${egg.value}` : `Egg ${egg.id}`}>
-                <img src={egg.image} alt="" aria-hidden="true" />
+            <button className="egg" onClick={() => onClick(egg.value)} aria-label={egg.clicked ? `Egg ${egg.id}, value: ${egg.value}` : `Egg ${egg.id}`}>
+                <picture>
+                    <source
+                        type="image/webp"
+                        srcSet={`${egg.image}.webp`}
+                    />
+                    <img
+                        src={`${egg.image}.png`}
+                        width={100}
+                        height={133}
+                        alt=""
+                        aria-hidden="true"
+                    />
+                </picture>
             </button>
         </li>
     )
